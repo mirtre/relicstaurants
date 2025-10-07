b6211159890351ee85e51899a4f43d84dfdf9eef
mirtre <miriamyflores@hotmail.com>
2025-10-07 15:51:00 -0700

    Update restaurants.js
    ec2-18-188-151-21.us-east-2.compute.amazonaws.com

--- src/views/restaurants/restaurants.js
+++ src/views/restaurants/restaurants.js
@@ -24,7 +24,7 @@ const Restaurants = () => {
   const [form] = Form.useForm();
 
   const getRestaurants = async () => {
-    const { data } = await axios.get('http://ip-172-31-34-181.us-east-2.compute.internal:3001/api/restaurants');
+    const { data } = await axios.get('http://ec2-18-188-151-21.us-east-2.compute.amazonaws.com:3001/api/restaurants');
     return data;
   };
   const { data } = useQuery('restaurants', getRestaurants);
