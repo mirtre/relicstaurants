b7ae2bbda29ac3b14e076db163693163de5b1944
mirtre <miriamyflores@hotmail.com>
2025-10-07 15:51:27 -0700

    Update single-restaurant.js

--- src/views/single-restaurant/single-restaurant.js
+++ src/views/single-restaurant/single-restaurant.js
@@ -13,7 +13,7 @@ import {
 } from './single-restaurant.styled';
 
 const getRestaurant = async (id) => {
-  const { data } = await axios.get(`http://ip-172-31-34-181.us-east-2.compute.internal:3003/api/menu/${id}`);
+  const { data } = await axios.get(`http://ec2-18-188-151-21.us-east-2.compute.amazonaws.com:3003/api/menu/${id}`);
 
   return data;
 };
