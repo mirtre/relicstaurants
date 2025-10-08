8aa9ede782e62934a0c331d5c7b0303b6ab4e118
Miriam Flores <mirmf1@gmail.com>
2025-10-07 19:44:46 -0700

    missed http

--- src/views/payments/payments.js
+++ src/views/payments/payments.js
@@ -16,7 +16,7 @@ const Payments = () => {
   const { Option } = Select;
 
   const handleOrderSending = async (payload) => {
-    await axios.post('lvh.me/api/checkout', payload);
+    await axios.post('http://lvh.me:3002/api/checkout', payload);
   };
 
   const columns = [
