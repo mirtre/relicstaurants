92252be3e67dc1baed2f8826318357d308c86c59
Miriam Flores <mirmf1@gmail.com>
2025-10-27 11:26:01 -0700

    new public DNS

--- src/views/payments/payments.js
+++ src/views/payments/payments.js
@@ -17,7 +17,7 @@ const Payments = () => {
 
   const handleOrderSending = async (payload) => {
     await axios.post(
-      'http://ec2-3-14-12-177.us-east-2.compute.amazonaws.com:3002/api/checkout',
+      'http://ec2-18-221-139-38.us-east-2.compute.amazonaws.com:3002/api/checkout',
       payload
     );
   };
